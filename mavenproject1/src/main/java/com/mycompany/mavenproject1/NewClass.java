cde0092fc7c4fc991d2ade208f0286ac9d4600d8
Chiranaka <166289915+Chiranaka@users.noreply.github.com>
2024-08-04 02:42:10 +0530

    create Octal to Binary method
    it is using array data structure to convert octal to binary in this method

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/NewClass.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/NewClass.java
@@ -4,13 +4,27 @@
  * and open the template in the editor.
  */
 package com.mycompany.mavenproject1;
+import java.util.*;
 
-/**
- *
- * @author viman
- */
+
 public class NewClass {
     
+    public int[] octalToBinary(int octal){
     
+        String octalString=Integer.toString(octal);//
+        int[] octalArray={};
+        for(int i=0;i<octalString.length();i++)
+        {
+            int digitValue=Integer.valueOf(octalString.charAt(i));
+            
+            octalArray[i]
+                   
+        }
+        Stack<Integer> octalInStack = new Stack<>();
+        
+    
+    
+    }
+       
     
 }
